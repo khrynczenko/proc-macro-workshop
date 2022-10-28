950bcda3b8a11cd4e8736abaa5f0c320a7eadf33
khrynczenko <jeniopy@gmail.com>
2022-10-28 19:02:22 +0200

    finish builder test 4, 5, 6

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,7 +2,33 @@ use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote;
 use syn;
-use syn::{DeriveInput, Ident, Type};
+use syn::{DeriveInput, GenericArgument, Ident, PathArguments, Type};
+
+fn is_type_option(ty: &Type) -> bool {
+    match ty {
+        Type::Path(tpath) => {
+            let last_segment = tpath.path.segments.last().unwrap();
+            let t_is_option = last_segment.ident.to_string() == String::from("Option");
+            if !t_is_option {
+                return false;
+            }
+            match &last_segment.arguments {
+                PathArguments::AngleBracketed(generics) => {
+                    if generics.args.len() != 1 {
+                        false
+                    } else {
+                        match generics.args.first().unwrap() {
+                            GenericArgument::Type(_) => true,
+                            _ => false,
+                        }
+                    }
+                }
+                _ => false,
+            }
+        }
+        _ => false,
+    }
+}
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -24,27 +50,62 @@ pub fn derive(input: TokenStream) -> TokenStream {
         _ => vec![],
     };
 
-    let field_idents: Vec<Ident> = fields.iter().map(|(i, _)| i.clone()).collect();
-    let field_tys: Vec<Type> = fields.iter().map(|(_, t)| t.clone()).collect();
+    let options: Vec<&(Ident, Type)> = fields.iter().filter(|(_, t)| is_type_option(t)).collect();
+    let regulars: Vec<&(Ident, Type)> = fields.iter().filter(|(_, t)| !is_type_option(t)).collect();
+
+    let option_idents: Vec<Ident> = options.iter().map(|(i, _)| i.clone()).collect();
+    let option_tys: Vec<Type> = options.iter().map(|(_, t)| t.clone()).collect();
+    let option_inner_tys: Vec<Type> = options
+        .iter()
+        .map(|(_, t)| match t {
+            Type::Path(tpath) => {
+                let last_segment = tpath.path.segments.last().unwrap();
+                match &last_segment.arguments {
+                    PathArguments::AngleBracketed(generics) => {
+                        match generics.args.first().unwrap() {
+                            GenericArgument::Type(inner_ty) => inner_ty.clone(),
+                            _ => panic!("impossible"),
+                        }
+                    }
+                    _ => panic!("impossible"),
+                }
+            }
+            _ => panic!("impossible"),
+        })
+        .collect();
+
+    let field_idents: Vec<Ident> = regulars.iter().map(|(i, _)| i.clone()).collect();
+    let field_tys: Vec<Type> = regulars.iter().map(|(_, t)| t.clone()).collect();
 
     let tokens = quote::quote!(
         use std::error::Error;
         impl #identifier {
             pub fn builder() -> #builder_identifier {
-                #builder_identifier { #(#field_idents : None),* }
+                #builder_identifier {
+                    #(#field_idents : None),*,
+                    #(#option_idents : None),*
+                }
             }
         }
 
         pub struct #builder_identifier {
-            #(#field_idents : Option<#field_tys>),*
+            #(#field_idents : Option<#field_tys>),*,
+            #(#option_idents : #option_tys),*
         }
 
         impl #builder_identifier {
             #(
-                pub fn #field_idents(&mut self, #field_idents: #field_tys) {
+                pub fn #field_idents(&mut self, #field_idents: #field_tys) -> &mut Self {
                     self.#field_idents = Some(#field_idents);
+                    self
                 }
+            )*
 
+            #(
+                pub fn #option_idents(&mut self, #option_idents: #option_inner_tys) -> &mut Self {
+                    self.#option_idents = Some(#option_idents);
+                    self
+                }
             )*
 
             pub fn build(&mut self) -> Result<#identifier, Box<dyn Error>> {
@@ -52,11 +113,12 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     return
                         Ok(
                             #identifier {
-                                #(#field_idents: self.#field_idents.clone().unwrap()),*
+                                #(#field_idents: self.#field_idents.clone().unwrap()),*,
+                                #(#option_idents: self.#option_idents.clone()),*
                             }
                         );
                 }
-                Err("xD".into())
+                Err("You must provide all required arguments!".into())
             }
 
         }
